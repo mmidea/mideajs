800c69fe5df41c1d340adcaabf0c4975a6359394
mmidea <mariq.razmirova@gmail.com>
2025-11-06 14:00:53 +0200

    dom

---
+++ 3Advanced/8/10. Contacts-Builder/myCode.js
@@ -0,0 +1,29 @@
+class Contact {
+    constructor(fName, lName, tel, email) {
+       this.firstName = fName;
+       this.lastName = lName;
+       this.phone = tel;
+       this.email = email; 
+       this.online = false;
+    }
+    render(id){
+        let newArticle = document.createElement("article");
+        newArticle.innerHTML += `<div class="title">${this.firstName} ${this.lastName}<button>&#8505;</button></div>`;
+        newArticle.innerHTML +=` <div class="info"><span>&phone; ${this.phone}</span><span>&#9993; ${this.email}</span></div>`
+
+        let mainElement = document.getElementById(id);
+        mainElement.appendChild(newArticle);
+    }
+}
+
+let contacts = [
+
+new Contact("Ivan", "Ivanov", "0888 123 456", "i.ivanov@gmail.com"),
+
+new Contact("Maria", "Petrova", "0899 987 654", "mar4eto@abv.bg"),
+
+new Contact("Jordan", "Kirov", "0988 456 789", "jordk@gmail.com")
+
+];
+
+//contacts.forEach(c => c.render('main')); 
--- 3Advanced/8/11. View-Model/app.js
+++ 3Advanced/8/11. View-Model/app.js
@@ -1,5 +1,31 @@
 class Textbox {
-    //TODO
+  _elements;
+        constructor(selector,regEx) {
+     this.value;
+     this._elements = document.querySelectorAll(selector) ;
+     
+     this._invalidSymbols = regEx;
+
+        
+    }
+    get elements(){
+        return this._elements
+    }
+    set elements(el){
+
+    }
+
+    isValid(){
+      let elementsArr =  Array.from(this._elements);
+      elementsArr.array.forEach(element => {
+        let textContent = element.textContent;
+        if(textContent.test(this._invalidSymbols)){
+            return false;
+        }
+      });
+      return true;
+
+    }
 }
 
 let textbox = new Textbox(".textbox",/[^a-zA-Z0-9]/);
