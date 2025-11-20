c2e747b365bf4d8c21a0b6627445834a5c79914e
mmidea <mariq.razmirova@gmail.com>
2025-11-20 11:54:05 +0200

    changes

---
+++ 4Applications/03.Gighub-Commits/app.js
@@ -0,0 +1,36 @@
+const list = document.getElementById("commits");
+
+function loadCommits() {
+    
+const username = document.getElementById("username").value;
+const repo = document.getElementById("repo").value;
+const url = `https://api.github.com/repos/${username}/${repo}/commits`
+
+fetch(url)
+.then(onHeaders)
+.then(displayCommits)
+.catch(onError)
+} 
+
+function onHeaders(response){
+    if(!response.ok){ throw "Error"}
+    return response.json();
+}
+
+function displayCommits(data){
+
+    //console.log(data.map(createListItem));
+    list.replaceChildren(...data.map(createListItem))
+    
+}
+
+function onError(error){
+    console.log(error);
+    
+}
+
+function createListItem({  commit: {author: {name}, message}   }){
+    const item = document.createElement("li");
+    item.textContent = `${name}: ${message}`;
+    return item;
+}
