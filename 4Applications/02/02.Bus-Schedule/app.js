d7d684af6d180cda656eb14a2e61f79f102f4a0e
mmidea <mariq.razmirova@gmail.com>
2025-11-21 15:42:09 +0200

    changes

--- 4Applications/02/02.Bus-Schedule/app.js
+++ 4Applications/02/02.Bus-Schedule/app.js
@@ -1,11 +1,56 @@
 function solve() {
+    let stopName = "Depot";
+    let stopId = "depot";
+    const messageRef = document.getElementsByClassName("info")[0];
+    const departBtnRef = document.getElementById("depart");
+    const arriveBtnRef = document.getElementById("arrive");
+        arriveBtnRef.disabled = true;
+    
 
-    function depart() {
-        console.log('Depart TODO...');
+
+    async function depart() {
+        
+
+        let url = `http://localhost:3030/jsonstore/bus/schedule/${stopId}`
+try {
+       let p = await fetch(url);
+       //debugger
+       if(p.status != 200){
+        throw new Error("Error");
+        
+       }
+        let body = await p.json();
+
+       
+        stopId = body.next;
+        stopName = body.name;
+    
+} catch (error) {
+    messageRef.textContent = "Error";
+    departBtnRef.disabled = true;
+    arriveBtnRef.disabled = true;  
+    return;
+      
+}
+        
+
+        
+
+        messageRef.textContent = `Next stop ${stopName}`;
+        departBtnRef.disabled = true;
+        arriveBtnRef.disabled = false;
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+
+
+
+
+
+        messageRef.textContent = `Arriving at ${stopName}`;
+        departBtnRef.disabled = false;
+        arriveBtnRef.disabled = true;
+        
     }
 
     return {
