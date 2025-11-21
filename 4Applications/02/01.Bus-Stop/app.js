c7a152cbca114a5272db484cbd5cc6a940e39a03
mmidea <mariq.razmirova@gmail.com>
2025-11-21 14:53:26 +0200

    changes

--- 4Applications/02/01.Bus-Stop/app.js
+++ 4Applications/02/01.Bus-Stop/app.js
@@ -1,29 +1,23 @@
-function getInfo() {
-    //console.log("TODO...");
+async function getInfo() {
+    
     const stopId = document.getElementById("stopId").value;
     const url = `http://localhost:3030/jsonstore/bus/businfo/${stopId}`;
    try {
-    myFunc(url);
-   } catch (error) {
-    document.getElementById("stopName").textContent  = "Error";
-   } 
-    
-    async function myFunc(url){
-
     const p = await fetch(url);
     const body = await p.json();
-    console.log(body);
-    return body;
-
-    }
+     
+    document.getElementById("buses").textContent = "";
     document.getElementById("stopName").textContent  = body.name;
     
     let arrayToProvide = Object.entries(body.buses).map(createElement);
-    console.log(arrayToProvide);
+    
     document.getElementById("buses").replaceChildren(...arrayToProvide);
     
+   } catch (error) {
+     document.getElementById("buses").textContent = "";
+    document.getElementById("stopName").textContent  = "Error";
+   } 
     
-
     function createElement(array){
        let element = document.createElement("li");
        element.textContent = `Bus ${array[0]} arrives in ${array[1]} minutes`;
